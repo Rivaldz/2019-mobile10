6b08647148ff265f0df9c98728712deda9f50e7c
Rivaldz <rivalzsetyo@gmail.com>
2019-10-28 14:51:47 +0700

    create view Model

--- SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/viewmodels/SharedScore.java
+++ SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/viewmodels/SharedScore.java
@@ -1,4 +1,43 @@
 package id.ac.polinema.skorbasket.viewmodels;
 
-public class SharedScore {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+public class SharedScore extends AndroidViewModel {
+    private MutableLiveData<Integer> scoreHome;
+    private MutableLiveData<Integer> scoreVisitor;
+    private MutableLiveData<Boolean> winner;
+    public SharedScore(@NonNull Application application) {
+        super(application);
+        scoreHome = new MutableLiveData<>();
+        scoreVisitor = new MutableLiveData<>();
+        winner = new MutableLiveData<>();
+    }
+
+    public MutableLiveData<Integer> getScoreHome() {
+        return scoreHome;
+    }
+
+    public void setScoreHome(MutableLiveData<Integer> scoreHome) {
+        this.scoreHome = scoreHome;
+    }
+
+    public MutableLiveData<Integer> getScoreVisitor() {
+        return scoreVisitor;
+    }
+
+    public void setScoreVisitor(MutableLiveData<Integer> scoreVisitor) {
+        this.scoreVisitor = scoreVisitor;
+    }
+
+    public MutableLiveData<Boolean> getWinner() {
+        return winner;
+    }
+
+    public void setWinner(MutableLiveData<Boolean> winner) {
+        this.winner = winner;
+    }
 }
